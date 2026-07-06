7fefb3ab63bc556c67747ccb75430f5c77e23ea5
Narngisa <nattanan43658@gmail.com>
2026-07-07 02:45:15 +0700

    Hello World

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,6 @@
 public class Main {
     public static void main() {
-        Customer.addCustomer();
-        Customer.addCustomer();
-        Customer.showAllCustomer();
-
-        Customer.setCustomer();
-        Customer.showAllCustomer();
-
-        Customer.delCustomer();
-        Customer.showAllCustomer();
-
-        Customer.closeScanner();
+        System.out.println("Hello World !!");
     }
 
 }
